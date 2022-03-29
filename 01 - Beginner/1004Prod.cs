c521610d13bf4977ff2adf23e969469c850ccfdb
fahmedx <fahmecanaldx@gmail.com>
2022-03-29 00:22:53 -0300

    Added Product

---
+++ 01 - Beginner/1004Prod.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int a,b,PROD;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        PROD = a*b;
+        Console.WriteLine("PROD = "+PROD);
+    }
+
+}
