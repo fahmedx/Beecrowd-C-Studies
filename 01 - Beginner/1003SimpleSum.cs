47480bd6b71bfd9230bf9310a25ac3b193d044a3
fahmedx <fahmecanaldx@gmail.com>
2022-03-29 00:15:03 -0300

    Added Sum

---
+++ 01 - Beginner/1003SimpleSum.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int a,b,sum;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        sum = a+b;
+        Console.WriteLine("SOMA = "+sum);
+    }
+
+}
