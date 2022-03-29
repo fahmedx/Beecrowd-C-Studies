ef5db08143985008e60b9122787f6e10611ced40
fahmedx <fahmecanaldx@gmail.com>
2022-03-28 23:22:33 -0300

    Extremely Basic added

---
+++ 01 - Beginner/1001ExtremelyBasic.cs
@@ -0,0 +1,11 @@
+using System; 
+class URI {
+    static void Main(string[] args) { 
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+        int X;
+        X=A+B;
+        Console.WriteLine("X = {0}",X);
+    }
+
+}
